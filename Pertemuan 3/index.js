8c9dfe5741f932cc4bcf1fb40e43e03b1b922257
Rionaldo G Pieterson Bagi <64066988+RionaldoBagi@users.noreply.github.com>
2020-11-16 22:49:19 +0800

    commit

---
+++ Pertemuan 3/index.js
@@ -0,0 +1,15 @@
+
+
+const firstName="Rionaldo"; 
+let age=20;
+const isMarried = true;
+
+console.log("My name is "+ firstName);
+console.log(age);
+console.log(isMarried);
+console.log("My name is "+ firstName +"I'm "+ age + " years old");
+
+// Validate the data type using typeof
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMarried);
